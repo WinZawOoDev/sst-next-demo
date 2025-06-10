0620854991e48e497c4dc207458fb1f55adaf5ee
root <winzaw.oo@kbzbank.com>
2025-06-10 09:17:30 +0000

    feat: blog cards ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,37 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] sm:item s-start">
-        <div className="">
-          <h1 className="text-5xl font-mono font-semibold">Space Flight,</h1>
-        </div>
-        <div className="border border-gray-200 rounded-md bg-white">
-          <h2 className="bg-gray-50 p-2.5">Card Header</h2>
-          <div className="p-3">
-            Card Content
-          </div>
-          <div className="bg-gray-50 p-2.5">
-            Card Footer
-          </div>
-        </div>
-      </main>
-    </div>
+    <main className="grid grid-cols-2 gap-16 min-h-screen mx-52 px-28 pb-20  sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+    </main>
   );
+}
+
+
+function BlogCard() {
+  return (
+    <section className="relative max-w-min-2xl rounded-md bg-white">
+      <div className="bg-gray-100 w-full min-h-[15em] items-center flex justify-center">
+        <span className="font-thin text-gray-500">Image</span>
+      </div>
+      <div className="relative flex items-center min-h-[7em] w-full mx-auto ">
+        <p className="line-clamp-3 text-center text-gray-700 max-w-2xl text-sm p-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, nisi. Voluptatum omnis, qui in nostrum asperiores cupiditate necessitatibus commodi consequatur temporibus laboriosam reprehenderit hic culpa illum reiciendis aliquam officiis quam!
+        </p>
+      </div>
+      <div className="relative w-full p-4">
+        <div className="w-fit text-gray-600 text-sm">
+          <span className="text-xs">Author:</span>
+          <span className="font-semibold text-gray-700 text-xs"> John Doe</span>
+        </div>
+        <div className="w-fit text-gray-600 text-sm">
+          <span className="text-xs">Date:</span>
+          <span className="font-semibold text-gray-700 text-xs"> 2023-10-01</span>
+        </div>
+      </div>
+    </section>
+  )
 }
